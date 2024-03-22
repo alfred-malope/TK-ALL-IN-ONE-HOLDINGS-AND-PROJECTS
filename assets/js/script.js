89c97af225db3b82a2a9ae6084a6693496914553
unknown <malopealfred12@gmail.com>
2024-03-22 19:22:50 +0200

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,128 +60,128 @@ function hideContact() {
     document.getElementById("popup-container").style.display = "none";
 }
 
-function fetchSkills() {
-    return fetch("./assets/json/skills.json")
-      .then(response => response.json())
-      .then(skillsData => {
-        return skillsData;
-      });
-  }
+// function fetchSkills() {
+//     return fetch("./assets/json/skills.json")
+//       .then(response => response.json())
+//       .then(skillsData => {
+//         return skillsData;
+//       });
+//   }
   
 
-function showSkills(skills) {
-    let skillsContainer = document.getElementById("skillsContainer");
-    let skillHTML = "";
-    skills.forEach(skill => {
-        skillHTML += `
-        <div class="bar">
-              <div class="info">
-                <img src=${skill.icon} alt="skill" />
-                <span>${skill.name}</span>
-              </div>
-            </div>`
-    });
-    skillsContainer.innerHTML = skillHTML;
-}
+// function showSkills(skills) {
+//     let skillsContainer = document.getElementById("skillsContainer");
+//     let skillHTML = "";
+//     skills.forEach(skill => {
+//         skillHTML += `
+//         <div class="bar">
+//               <div class="info">
+//                 <img src=${skill.icon} alt="skill" />
+//                 <span>${skill.name}</span>
+//               </div>
+//             </div>`
+//     });
+//     skillsContainer.innerHTML = skillHTML;
+// }
 
 
-fetchSkills().then(data => {
-    showSkills(data);
-});
+// fetchSkills().then(data => {
+//     showSkills(data);
+// });
 
 
 
-function fetchProjects() {
-    return fetch("./assets/json/projects.json")
-      .then(response => response.json())
-      .then(projectsData => {
-        return projectsData;
-      });
-  }
+// function fetchProjects() {
+//     return fetch("./assets/json/projects.json")
+//       .then(response => response.json())
+//       .then(projectsData => {
+//         return projectsData;
+//       });
+//   }
   
 
-function showProjects(projects) {
-    let projectsContainer = document.getElementById("projects-card");
-    let projectHTML = "";
-    projects.forEach(project => {
-        projectHTML += `
-        <article class="projects-card" >
-        <div class="projects-image">
-            <img src="${project.photo}" class="projects-img" alt="image">
+// function showProjects(projects) {
+//     let projectsContainer = document.getElementById("projects-card");
+//     let projectHTML = "";
+//     projects.forEach(project => {
+//         projectHTML += `
+//         <article class="projects-card" >
+//         <div class="projects-image">
+//             <img src="${project.photo}" class="projects-img" alt="image">
 
-            <a href="${project.link}" target="_blank" class="projects-button button">
-                <i class="ri-arrow-right-up-line"></i>
-            </a>
-        </div>
+//             <a href="${project.link}" target="_blank" class="projects-button button">
+//                 <i class="ri-arrow-right-up-line"></i>
+//             </a>
+//         </div>
 
-        <div class="projects-content">
-            <h3 class="projects-subtitle">Website</h3>
-            <h2 class="projects-title">${project.title}</h2>
+//         <div class="projects-content">
+//             <h3 class="projects-subtitle">Website</h3>
+//             <h2 class="projects-title">${project.title}</h2>
 
-            <p class="projects-description">${project.description}</p>
-        </div>
+//             <p class="projects-description">${project.description}</p>
+//         </div>
 
-        <div class="projects-buttons">
-            <a href="${project.link}" target="_blank" class="projects-link">
-                <i class="ri-github-line"> Live</i>
-            </a>
-            <a href="${project.github}" target="_blank" class="projects-link">
-                <i class="ri-github-line"> Code</i>
-            </a>
+//         <div class="projects-buttons">
+//             <a href="${project.link}" target="_blank" class="projects-link">
+//                 <i class="ri-github-line"> Live</i>
+//             </a>
+//             <a href="${project.github}" target="_blank" class="projects-link">
+//                 <i class="ri-github-line"> Code</i>
+//             </a>
 
-        </div>
-        </article>`
-    });
-    projectsContainer.innerHTML = projectHTML;
-}
-
-
-fetchProjects().then(data => {
-    showProjects(data);
-});
+//         </div>
+//         </article>`
+//     });
+//     projectsContainer.innerHTML = projectHTML;
+// }
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    const currentYear = new Date().getFullYear();
-    document.getElementById('currentYear').innerText = currentYear;
-});
+// fetchProjects().then(data => {
+//     showProjects(data);
+// });
 
-const slides = document.querySelector(".slider").children;
-const indicatorImages = document.querySelector(".slider-indicator").children;
 
-for(let i=0; i<indicatorImages.length; i++){
-  indicatorImages[i].addEventListener("click", function(){
+// document.addEventListener('DOMContentLoaded', function () {
+//     const currentYear = new Date().getFullYear();
+//     document.getElementById('currentYear').innerText = currentYear;
+// });
 
-    for(let j=0; j<indicatorImages.length; j++){
-      indicatorImages[j].classList.remove("active");
-    }
-    this.classList.add("active");
-    const id=this.getAttribute("data-id");
-    for(let j=0; j<slides.length; j++){
-      slides[j].classList.remove("active");
-    }
+// const slides = document.querySelector(".slider").children;
+// const indicatorImages = document.querySelector(".slider-indicator").children;
 
-    slides[id].classList.add("active");
-  })
-}
+// for(let i=0; i<indicatorImages.length; i++){
+//   indicatorImages[i].addEventListener("click", function(){
 
-function infoReveal(elementId, clickedLink) {
-  const allRevealTexts = document.querySelectorAll('.revealImage');
-  allRevealTexts.forEach(text => {
-      text.style.display = "none";
-  });
+//     for(let j=0; j<indicatorImages.length; j++){
+//       indicatorImages[j].classList.remove("active");
+//     }
+//     this.classList.add("active");
+//     const id=this.getAttribute("data-id");
+//     for(let j=0; j<slides.length; j++){
+//       slides[j].classList.remove("active");
+//     }
 
-  const revealText = document.getElementById(elementId);
-  if (revealText) {
-      revealText.style.display = "flex";
-  }
+//     slides[id].classList.add("active");
+//   })
+// }
 
-  const allLinks = document.querySelectorAll('.about-header a');
-  allLinks.forEach(link => {
-      link.classList.remove('active');
-  });
-  if (clickedLink) {
-      clickedLink.classList.add('active');
-  }
-};
+// function infoReveal(elementId, clickedLink) {
+//   const allRevealTexts = document.querySelectorAll('.revealImage');
+//   allRevealTexts.forEach(text => {
+//       text.style.display = "none";
+//   });
+
+//   const revealText = document.getElementById(elementId);
+//   if (revealText) {
+//       revealText.style.display = "flex";
+//   }
+
+//   const allLinks = document.querySelectorAll('.about-header a');
+//   allLinks.forEach(link => {
+//       link.classList.remove('active');
+//   });
+//   if (clickedLink) {
+//       clickedLink.classList.add('active');
+//   }
+// };
 
